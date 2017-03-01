f47ac3a7923814b3b1119d608185c5e26cecf82b
Johan Ström <johan@stromnet.se>
2017-03-01 20:07:44 +0100

    C++11 std::next fixes for stdlibc++ (Linux)

--- core/resolver/agoresolver.cpp
+++ core/resolver/agoresolver.cpp
@@ -114,7 +114,7 @@ public:
 };
 
 // helper to determine last element
-#ifndef _LIBCPP_ITERATOR
+#if !defined(_LIBCPP_ITERATOR) and __cplusplus < 201103L
     template <typename Iter>
 Iter next(Iter iter)
 {
