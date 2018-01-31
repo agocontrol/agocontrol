523de7e57a8a9ff0dd27d40b617f33d443cc65e2
Johan Ström <johan@stromnet.se>
2018-01-31 21:53:56 +0100

    rcp/html: rework how we copy non-templated html/ files
    Before files where only copied during initial cmake run, making development cumbersome.
    Now, "make" will re-copy any changed files

--- core/rpc/mergetranslation.sh
+++ core/rpc/mergetranslation.sh
@@ -1,35 +1,36 @@
 #!/usr/bin/env bash
-TMPFILE=$(mktemp -u).xml.in
+set -e 
+
 SOURCEFILE=$(realpath $1)
-ln -s ${SOURCEFILE} ${TMPFILE} || exit 1
-intltool-merge -x html/po ${TMPFILE} $2 || exit 1
+OUTFILE=$2
+
+mkdir -p $(dirname $OUTFILE)
+intltool-merge -q -x html/po ${SOURCEFILE} ${OUTFILE}
 
 # Remove first line: <?xml version...
 # sed -i is not POSIX, and behaves differently on BSD vs GNU
 if [ `uname -s` == "FreeBSD" ]; then
-	sed -i '' '1d' $2 || exit 1
+	sed -i '' '1d' ${OUTFILE}
 else
-	sed -i '1d' $2 || exit 1
+	sed -i '1d' ${OUTFILE}
 fi
-
-rm ${TMPFILE}
 
 # Restore knockout containerless control flow
 # And replace all "and" and "or" respectively by "&&" and "||". Only 3 substitutions are supported !
-TMPFILE2="${SOURCEFILE}.tmp"
+TMPFILE="${OUTFILE}.tmp"
 sed -e '/<ko opts=".*">/{s/\(.*\) and \(.*\)/\1 \&\& \2/gi
 	}' \
 	 -e '/<ko opts=".*">/{s/\(.*\) and \(.*\)/\1 \&\& \2/gi
 	}' \
 	 -e '/<ko opts=".*">/{s/\(.*\) and \(.*\)/\1 \&\& \2/gi
-	}' $2 > $TMPFILE2 && mv $TMPFILE2 $2 || exit 1
+	}' $2 > $TMPFILE && mv $TMPFILE ${OUTFILE}
 
 sed -e '/<ko opts=".*">/{s/\(.*\) or \(.*\)/\1 \|\| \2/gi
 	}' \
     -e '/<ko opts=".*">/{s/\(.*\) or \(.*\)/\1 \|\| \2/gi
 	}' \
     -e '/<ko opts=".*">/{s/\(.*\) or \(.*\)/\1 \|\| \2/gi
-	}' $2 > $TMPFILE2 && mv $TMPFILE2 $2 || exit 1
+	}' $2 > $TMPFILE && mv $TMPFILE ${OUTFILE}
 
-sed -e "s/<ko opts=\"\(.*\)\">/<\!\-\- ko \1 \-\->/g" $2 > $TMPFILE2 && mv $TMPFILE2 $2 || exit 1
-sed -e "s/<\/ko>/<\!\-\- \/ko \-\->/g" $2 > $TMPFILE2 && mv $TMPFILE2 $2 || exit 1
+sed -e "s/<ko opts=\"\(.*\)\">/<\!\-\- ko \1 \-\->/g" ${OUTFILE} > $TMPFILE && mv $TMPFILE ${OUTFILE}
+sed -e "s/<\/ko>/<\!\-\- \/ko \-\->/g" ${OUTFILE} > $TMPFILE && mv $TMPFILE ${OUTFILE}
