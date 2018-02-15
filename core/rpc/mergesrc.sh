d60466269ee7c89dec9df0c81178bd370f8608f0
Johan Ström <johan@stromnet.se>
2018-02-15 20:21:38 +0100

    UI: load merged CSS & JS files instead of 11 & 28 individual ones...
    Very crude and ugly mergeprocess but no new dependencies

---
+++ core/rpc/mergesrc.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+OUTFILE=$1
+shift
+
+rm -f $OUTFILE
+
+mkdir -p $(dirname $OUTFILE)
+touch $OUTFILE
+for INFILE in $*; do
+    echo "Adding into $OUTFILE: $(basename $INFILE)"
+    echo "/* BEGIN $(basename $INFILE) */" >> $OUTFILE
+    cat $INFILE >> $OUTFILE
+    echo "/* END $(basename $INFILE) */" >> $OUTFILE
+done
