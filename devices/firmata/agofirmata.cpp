1816b10bf44d33fcf65fb693cf828b0a4da9a760
Johan Ström <johan@stromnet.se>
2018-03-11 11:53:10 +0100

    Convert Variant->Json, devices/firmata

--- devices/firmata/agofirmata.cpp
+++ devices/firmata/agofirmata.cpp
@@ -10,29 +10,42 @@ using namespace agocontrol;
 
 class AgoFirmata: public AgoApp {
 private:
-    Firmata* f;
+    std::unique_ptr<Firmata> f;
 
-    qpid::types::Variant::Map commandHandler(qpid::types::Variant::Map content);
+    Json::Value commandHandler(const Json::Value& content);
     void setupApp();
     void cleanupApp();
 public:
     AGOAPP_CONSTRUCTOR(AgoFirmata);
 };
 
-qpid::types::Variant::Map AgoFirmata::commandHandler(qpid::types::Variant::Map content) {
-    qpid::types::Variant::Map returnval;
-    int pin = atoi(content["internalid"].asString().c_str());
+Json::Value AgoFirmata::commandHandler(const Json::Value& content) {
+    checkMsgParameter(content, "command", Json::stringValue);
+    checkMsgParameter(content, "internalid");
+
+    std::string command = content["command"].asString();
+    std::string internalid = content["internalid"].asString();
+
+    Json::Value returnval;
+    int pin;
+    if(!stringToInt(content["internalid"], pin))
+        return responseError(RESPONSE_ERR_PARAMETER_INVALID, "internalid must be integer");
+
     int result;
     if (content["command"] == "on" ) {
-        result = f->writeDigitalPin(pin,ARDUINO_HIGH);
+        result = f->writeDigitalPin(pin, ARDUINO_HIGH);
         // TODO: send proper status events
     } else if (content["command"] == "off") {
-        result = f->writeDigitalPin(pin,ARDUINO_LOW);
+        result = f->writeDigitalPin(pin, ARDUINO_LOW);
     }
+    else
+        return responseUnknownCommand();
+
     if (result >= 0)
     {
         return responseSuccess();
     }
+
     return responseError(RESPONSE_ERR_INTERNAL, "Cannot set firmata digital pin");
 }
 
@@ -41,7 +54,7 @@ void AgoFirmata::setupApp() {
     std::string devicefile=getConfigOption("device", "/dev/ttyUSB2");
     std::stringstream outputs(getConfigOption("outputs", "2")); // read digital out pins from config, default to pin 2 only
 
-    f = new Firmata();
+    f.reset(new Firmata());
     if (f->openPort(devicefile.c_str()) != 0) {
         AGO_FATAL() << "cannot open device: " << devicefile;
         f->destroy();
